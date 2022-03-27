f736709bf2e8cda53a3018385f642903e400fd8b
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-27 14:19:08 +0200

    updated

--- src/components/login/MyLogin.jsx
+++ src/components/login/MyLogin.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -17,6 +16,9 @@ import "./style.css";
 import GoogleIcon from "@mui/icons-material/Google";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import AppleIcon from "@mui/icons-material/Apple";
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationIcon } from "@heroicons/react/outline";
 
 const theme = createTheme();
 
@@ -24,6 +26,11 @@ export default function MyLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
+  const [open, setOpen] = useState(false);
+
+  const cancelButtonRef = useRef(null);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -44,7 +51,9 @@ export default function MyLogin() {
         body: JSON.stringify(newPost),
         headers: { "Content-type": "application/json" },
       });
-      if (res.status !== 200) alert("you you entered wrong password or email");
+      if (res.status !== 200) {
+        setOpen(true);
+      }
       if (res.ok) {
         let data = await res.json();
         localStorage.setItem("MyToken", data.accessToken);
@@ -57,129 +66,220 @@ export default function MyLogin() {
   };
 
   return (
-    <ThemeProvider className="main-theme-login" theme={theme}>
-      <Grid container component="main" sx={{ height: "82vh", zIndex: "-1" }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: `url(${image})`,
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid
-          item
-          xs={12}
-          sm={8}
-          md={5}
-          className="shadow-needed-login shadow-none"
-          component={Paper}
-          elevation={6}
-          square
-        >
-          <Box
+    <div>
+      <ThemeProvider className="main-theme-login" theme={theme}>
+        <Grid container component="main" sx={{ height: "82vh", zIndex: "-1" }}>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
             sx={{
-              mt: 6,
-              mb: 10,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+              backgroundImage: `url(${image})`,
+              backgroundRepeat: "no-repeat",
+              backgroundColor: (t) =>
+                t.palette.mode === "light"
+                  ? t.palette.grey[50]
+                  : t.palette.grey[900],
+              backgroundSize: "cover",
+              backgroundPosition: "center",
             }}
+          />
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            md={5}
+            className="shadow-needed-login shadow-none"
+            component={Paper}
+            elevation={6}
+            square
           >
-            <Avatar sx={{ m: 1, bgcolor: "#6c63ff" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
             <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+              sx={{
+                mt: 6,
+                mb: 10,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
             >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                id="password"
-                autoComplete="current-password"
-              />
+              <Avatar sx={{ m: 1, bgcolor: "#6c63ff" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  id="password"
+                  autoComplete="current-password"
+                />
 
-              <div className="d-flex">
-                <a href="http://localhost:3001/users/googleLogin">
+                <div className="d-flex">
+                  <a href="http://localhost:3001/users/googleLogin">
+                    <Button
+                      className="mr-1 px-5 text-dark social-media-login"
+                      fullWidth
+                      sx={{ mt: 3, mb: 2 }}
+                    >
+                      <GoogleIcon />
+                    </Button>
+                  </a>
+
                   <Button
-                    className="mr-1 px-5 text-dark social-media-login"
+                    className="mx-1 text-dark social-media-login"
                     fullWidth
                     sx={{ mt: 3, mb: 2 }}
                   >
-                    <GoogleIcon />
+                    <FacebookIcon />
                   </Button>
-                </a>
-
+                  <Button
+                    className="ml-1 text-dark social-media-login"
+                    fullWidth
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    <AppleIcon />
+                  </Button>
+                </div>
                 <Button
-                  className="mx-1 text-dark social-media-login"
+                  type="submit"
+                  className="ml-1"
                   fullWidth
-                  sx={{ mt: 3, mb: 2 }}
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, bgcolor: "#6c63ff" }}
+                  disabled={!email || !password}
+                  onClick={() => handleRegister()}
                 >
-                  <FacebookIcon />
+                  Sign In
                 </Button>
-                <Button
-                  className="ml-1 text-dark social-media-login"
-                  fullWidth
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  <AppleIcon />
-                </Button>
-              </div>
-              <Button
-                type="submit"
-                className="ml-1"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, bgcolor: "#6c63ff" }}
-                disabled={!email || !password}
-                onClick={() => handleRegister()}
-              >
-                Sign In
-              </Button>
 
-              <Grid container>
-                <Grid item>
-                  <Link to="/register">{"Don't have an account? Sign Up"}</Link>
+                <Grid container>
+                  <Grid item>
+                    <Link to="/register">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
                 </Grid>
-              </Grid>
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </ThemeProvider>
+      </ThemeProvider>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed z-10 inset-0  modal-sign-up-error"
+          initialFocus={cancelButtonRef}
+          onClose={setOpen}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg leading-6 font-medium text-gray-900  deactivate-account"
+                      >
+                        Sign in Error!
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500 deactivate-account">
+                          An account with that email address already exists.
+                          Please use a unique email.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 modal-2-buttons px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2  sm:ml-3 sm:w-auto sm:text-sm login-in-sing-up-modal"
+                    onClick={() => {
+                      setOpen(false);
+                    }}
+                  >
+                    Try again
+                  </button>
+                  <button
+                    type="button"
+                    className=" w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => {
+                      navigate("/register");
+                      setOpen(false);
+                    }}
+                  >
+                    Register
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </div>
   );
 }
