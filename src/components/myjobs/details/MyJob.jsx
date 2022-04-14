d6205f1372d8481859659fd18bd8fa418c08331e
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-04-14 10:58:41 +0200

    updated

--- src/components/myjobs/details/MyJob.jsx
+++ src/components/myjobs/details/MyJob.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Form } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import SingleJobMap from "../../myMap/SingleJobMap";
 import CompImage from "./CompImage";
 import Button from "@mui/material/Button";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import MyLoader from "../../loader/MyLoader";
 import WorkIcon from "@mui/icons-material/Work";
@@ -13,23 +13,27 @@ import RemoveCircleRoundedIcon from "@mui/icons-material/RemoveCircleRounded";
 import Tooltip from "@mui/material/Tooltip";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import "./style.css";
-import TextField from "@mui/material/TextField";
-import MyJobTech from "./MyJobTech";
+import JobStats from "./JobStats";
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
+import AllApplicants from "./AllApplicants";
 
 function MyJob() {
+  const navigate = useNavigate();
   const params = useParams();
   const [job, setJob] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const userMe = useSelector((state) => state.userMe);
+  const [footer, setFooter] = useState(false);
 
-  //   update
+  const extraHeader = () => {
+    if (window.scrollY >= 200) {
+      setFooter(true);
+    } else {
+      setFooter(false);
+    }
+  };
 
-  const [salary, setSalary] = useState("");
-  const [title, setTitle] = useState("");
-  const [type, setType] = useState("");
-  const [location, setLocation] = useState("");
-  const [experience, setExperience] = useState("");
-
+  window.addEventListener("scroll", extraHeader);
   const fetchJob = async () => {
     try {
       let res = await fetch(
@@ -56,61 +60,30 @@ function MyJob() {
         <MyLoader />
       ) : (
         <Row className="m-3 p-0" sm={1} md={2}>
-          <Col className="p-0" md={8}>
+          <Col className="" md={8}>
             <Row className="job-title-comp-info-row m-0">
               <Row className="w-100 m-3 align-items-center justify-content-between">
                 <div className="d-flex align-items-center">
                   <CompImage job={job.companyName} />
                   <div className="ml-3">
-                    <Form.Group controlId="formBasicSalary">
-                      <Form.Control
-                        style={{ fontSize: "30px", fontWeight: "600" }}
-                        className="p-0 border-0 shadow-none"
-                        type="text"
-                        placeholder="Title"
-                        defaultValue={job.title}
-                        onChange={(e) => {
-                          setTitle(e.target.value);
-                        }}
-                      />
-                    </Form.Group>
+                    <h2 className="job-details-hob-title mb-1">{job.title}</h2>
                     <h6 className="job-details-hob-company-name mb-2">
                       {job.companyName}
                     </h6>
-
-                    <Form.Group controlId="formBasicSalary">
-                      <Form.Control
-                        className="p-0 border-0 shadow-none"
-                        type="text"
-                        placeholder="Salary"
-                        defaultValue={job.salary}
-                        onChange={(e) => {
-                          setSalary(e.target.value);
-                        }}
-                      />
-                    </Form.Group>
-                  </div>
-                </div>
-                <div>
-                  <div>
-                    <Tooltip title="Delete" placement="left">
-                      <Button
-                        className="apply-job-delete-btn"
-                        variant="contained"
-                      >
-                        DELETE
-                      </Button>
-                    </Tooltip>
-                  </div>
-                  <Tooltip title="Update" placement="left">
-                    <Button
-                      className="apply-job-update-btn mt-2"
-                      variant="contained"
+                    <h6
+                      className="mb-0 text-muted"
+                      style={{ fontSize: "12px" }}
                     >
-                      UPDATE
-                    </Button>
-                  </Tooltip>
+                      SALARY
+                    </h6>
+                    <h6 style={{ fontSize: "17px" }}>${job.salary}</h6>
+                  </div>
                 </div>
+                <Tooltip title="Apply" placement="left">
+                  <Button className="delete-job-apply-btn" variant="contained">
+                    DELETE
+                  </Button>
+                </Tooltip>
               </Row>
               <Row className="m-3 w-100 justify-content-between">
                 <div className="d-flex align-items-center">
@@ -118,33 +91,22 @@ function MyJob() {
                     <h6 className="text-muted" style={{ fontSize: "12px" }}>
                       EMPLOYMENT TYPE
                     </h6>
-                    <Form.Group controlId="formBasicSalary">
-                      <Form.Control
-                        className="p-0 border-0 shadow-none"
-                        type="text"
-                        placeholder="Type"
-                        defaultValue={job.type}
-                        onChange={(e) => {
-                          setType(e.target.value);
-                        }}
-                      />
-                    </Form.Group>
+                    <h6 className="d-flex align-items-center">
+                      <WorkIcon className="mr-2" style={{ color: "#564de5" }} />
+                      {job.type}
+                    </h6>
                   </div>
                   <div style={{ marginLeft: "8vw" }}>
                     <h6 className="text-muted" style={{ fontSize: "12px" }}>
                       LOCATION
                     </h6>
-                    <Form.Group controlId="formBasicSalary">
-                      <Form.Control
-                        className="p-0 border-0 shadow-none "
-                        type="text"
-                        placeholder="Location"
-                        defaultValue={job.location}
-                        onChange={(e) => {
-                          setLocation(e.target.value);
-                        }}
+                    <h6 className="d-flex align-items-center">
+                      <LocationOnRoundedIcon
+                        className="mr-2"
+                        style={{ color: "#564de5" }}
                       />
-                    </Form.Group>
+                      {job.location}
+                    </h6>
                   </div>
                 </div>
 
@@ -152,21 +114,36 @@ function MyJob() {
                   <h6 className="text-muted" style={{ fontSize: "12px" }}>
                     EXPERIENCE
                   </h6>
-                  <Form.Group controlId="formBasicSalary">
-                    <Form.Control
-                      className="p-0 border-0 mr-0 shadow-none ml-auto"
-                      type="text"
-                      placeholder="Experience"
-                      defaultValue={job.experience}
-                      onChange={(e) => {
-                        setExperience(e.target.value);
-                      }}
+                  <h6 className="d-flex align-items-center">
+                    <Filter2RoundedIcon
+                      className="mr-2"
+                      style={{ color: "#564de5" }}
                     />
-                  </Form.Group>
+                    {job.experience}
+                  </h6>
                 </div>
               </Row>
               <Row className="m-3 w-100">
-                <MyJobTech job={job} />
+                <div>
+                  <h6 className="text-muted" style={{ fontSize: "12px" }}>
+                    TECHNOLOGIES
+                  </h6>
+                  <div className="d-flex align-items-center">
+                    {job.techStack.length !== 0 ? (
+                      job.techStack.map((j) => {
+                        return (
+                          <Tooltip title={j.skill} placement="top">
+                            <h6 className="singe-job-all-skills">{j.skill}</h6>
+                          </Tooltip>
+                        );
+                      })
+                    ) : (
+                      <h6 className="">
+                        <RemoveCircleRoundedIcon style={{ color: "#564de5" }} />
+                      </h6>
+                    )}
+                  </div>
+                </div>
               </Row>
               <Row className="m-3 w-100 flex-column justify-content-center">
                 <h6 className="text-muted" style={{ fontSize: "12px" }}>
@@ -233,11 +210,53 @@ function MyJob() {
                   </div>
                 </div>
               </Row>
+              <Row className="statistics-down-btn-row m-0 p-0 pr-3 mb-4">
+                <JobStats applicants={job.applicants} />
+              </Row>
+              {footer ? (
+                <Row className="apply-down-btn-row m-0 p-0 px-3 justify-content-between w-100">
+                  <ArrowBackOutlinedIcon
+                    className="go-back-arrow"
+                    onClick={() => navigate("/")}
+                  />
+                  <div className="d-flex align-items-center ml-3">
+                    {job.techStack.length !== 0 ? (
+                      job.techStack.slice(0, 3).map((j) => {
+                        return (
+                          <Tooltip title={j.skill} placement="top">
+                            <h6 className="singe-job-all-skills-down mb-0">
+                              {j.skill}
+                            </h6>
+                          </Tooltip>
+                        );
+                      })
+                    ) : (
+                      <h6 className="">
+                        <RemoveCircleRoundedIcon style={{ color: "#564de5" }} />
+                      </h6>
+                    )}
+                  </div>
+                  <h6 className="mb-0 text-light ml-auto mr-3">
+                    ${job.salary}
+                  </h6>
+                  <Tooltip title="Apply" placement="top">
+                    <Button
+                      className="delete-job-apply-btn-down"
+                      variant="contained"
+                    >
+                      DELETE
+                    </Button>
+                  </Tooltip>
+                </Row>
+              ) : (
+                ""
+              )}
             </Row>
           </Col>
-          <Col className="col-for-map-and-apply p-0" md={4}>
-            <Row className="col-2-for-map-main-div m-0 mb-2 mt-n1"></Row>
-            <Row className="col-2-for-map-2nd-div m-0">
+
+          <Col className="col-for-map-and-apply " md={4}>
+            <Row className="col-2-for-map-main-div m-0 mt-n1"></Row>
+            <Row className="col-2-for-map-2nd-div m-0 mt-1">
               <SingleJobMap />
             </Row>
           </Col>
